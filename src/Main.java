b1190f23ef73290a0d1e25e2e749889ac78238f8
Luiz Filipe <luiz.lopes@get.inatel.br>
2023-02-10 09:20:45 -0300

    Um comentário e outro System out

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá Java!");
+        //teste1234
+        System.out.println("Teste1234");
     }
 
 }
